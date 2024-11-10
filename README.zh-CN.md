f48b29ec7928626d307511489952a449fb4998b8
Right2022 <3118138007@qq.com>
2024-11-11 00:54:58 +0800

    update

---
+++ README.zh-CN.md
@@ -0,0 +1,71 @@
+#  React 应用程序 —— 播放器
+[英文](README.md)
+---
+本项目通过 [Create React App](https://github.com/facebook/create-react-app)启动。
+
+## 可用脚本
+
+在项目目录下，你可以运行
+
+### `npm start`
+
+以开发模式运行应用程序。
+打开 [http://localhost:3000](http://localhost:3000)，在浏览器中查看。
+
+当你进行更改时，页面会重新加载。
+你还可以在控制台中看到任何 lint 错误。
+
+###`npm test``
+
+以交互式观察模式启动测试运行器。
+更多信息请参阅[运行测试](https://facebook.github.io/create-react-app/docs/running-tests)部分。
+
+### `npm run build`.
+
+在 `build` 文件夹中构建用于生产的应用程序。
+它会在生产模式下正确捆绑 React，并优化构建以获得最佳性能。
+
+编译后的文件将被最小化，文件名包含哈希值。
+您的应用程序已准备就绪，可以部署了！
+
+更多信息，请参阅 [部署](https://facebook.github.io/create-react-app/docs/deployment) 部分。
+
+### `npm run eject`.
+
+**注意：这是一个单向操作。一旦 “弹出”，就无法返回！ **
+
+如果你对构建工具和配置选择不满意，可以随时`eject`。该命令将删除项目中的单一构建依赖关系。
+
+相反，它会将所有配置文件和传递依赖（webpack、Babel、ESLint 等）直接复制到你的项目中，因此你可以完全控制它们。除了 “eject ”之外，其他所有命令都将继续工作，但它们将指向复制的脚本，因此你可以对它们进行调整。在这一点上，你就可以独立操作了。
+
+你不必再使用 `eject`。精心策划的功能集适用于中小型部署，你不应该觉得必须使用这一功能。不过我们知道，如果您不能在准备就绪时对其进行自定义，那么该工具就不会有用。
+
+## 了解更多
+
+您可以在 [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started) 中了解更多信息。
+
+要学习 React，请查看 [React 文档](https://reactjs.org/)。
+
+### 代码拆分
+
+本节移至此处： [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+
+### 分析软件包大小
+
+本节移至此处： [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
+
+### 制作渐进式 Web 应用程序
+
+本节已移至此处： [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
+
+### 高级配置
+
+本节移至此处： [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
+
+### 部署
+
+本节移至此处： [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
+
+### `npm run build` 无法最小化
+
+本节移至此处： [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,247 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//ÒôÀÖ²¥·Å
+
+// import React from 'react';
+// import ReactAplayer from 'react-aplayer';
+
+// export default class App extends React.Component {
+//   // event binding example
+//   onPlay = () => {
+//     console.log('on play');
+//   };
+
+//   onPause = () => {
+//     console.log('on pause');
+//   };
+
+//   // example of access aplayer instance
+//   onInit = ap => {
+//     this.ap = ap;
+//   };
+
+//   render() {
+//     const props = {
+//       theme: '#F57F17',
+//       lrcType: 3,
+//       audio: [
+//         {
+//           name: '¹â¤ë¤Ê¤é',
+//           artist: 'Goose house',
+//           url: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.mp3',
+//           cover: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.jpg',
+//           lrc: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.lrc',
+//           theme: '#ebd0c2'
+//         }
+//       ]
+//     };
+
+//     return (
+//       <div>
+//         <ReactAplayer
+//           {...props}
+//           onInit={this.onInit}
+//           onPlay={this.onPlay}
+//           onPause={this.onPause}
+//         />
+//         {/* example of access aplayer instance API */}
+//         <button onClick={() => this.ap.toggle()}>toggle</button>
+//       </div>
+//     );
+//   }
+// }
+
+import "./App.css";
+import ReactPlayer from "react-player";
+import { Container } from "@material-ui/core";
+import Control from "./Components/Control";
+import { useState, useRef } from "react";
+import { formatTime } from "./format";
+
+let count = 0;
 function App() {
+  const videoPlayerRef = useRef(null);
+  const controlRef = useRef(null);
+
+  const [videoState, setVideoState] = useState({
+    playing: false,
+    muted: false,
+    volume: 0.5,
+    playbackRate: 1.0,
+    played: 0,
+    seeking: false,
+    buffer: true,
+  });
+
+  //Destructuring the properties from the videoState
+  const { playing, muted, volume, playbackRate, played, seeking, buffer } =
+    videoState;
+
+  const currentTime = videoPlayerRef.current
+    ? videoPlayerRef.current.getCurrentTime()
+    : "00:00";
+  const duration = videoPlayerRef.current
+    ? videoPlayerRef.current.getDuration()
+    : "00:00";
+
+  const formatCurrentTime = formatTime(currentTime);
+  const formatDuration = formatTime(duration);
+
+  const playPauseHandler = () => {
+    //plays and pause the video (toggling)
+    setVideoState({ ...videoState, playing: !videoState.playing });
+  };
+
+  const rewindHandler = () => {
+    //Rewinds the video player reducing 5
+    videoPlayerRef.current.seekTo(videoPlayerRef.current.getCurrentTime() - 5);
+  };
+
+  const handleFastFoward = () => {
+    //FastFowards the video player by adding 10
+    videoPlayerRef.current.seekTo(videoPlayerRef.current.getCurrentTime() + 10);
+  };
+
+  //console.log("========", (controlRef.current.style.visibility = "false"));
+  const progressHandler = (state) => {
+    if (count >= 0) {
+      console.log("close");
+      controlRef.current.style.visibility = "hidden"; // toggling player control container
+    } else if (controlRef.current.style.visibility === "visible") {
+      count += 1;
+    }
+
+    if (!seeking) {
+      setVideoState({ ...videoState, ...state });
+    }
+  };
+
+  const seekHandler = (e, value) => {
+    setVideoState({ ...videoState, played: parseFloat(value / 100) });
+    videoPlayerRef.current.seekTo(parseFloat(value / 100));
+  };
+
+  const seekMouseUpHandler = (e, value) => {
+    console.log(value);
+
+    setVideoState({ ...videoState, seeking: false });
+    videoPlayerRef.current.seekTo(value / 100);
+  };
+
+  const volumeChangeHandler = (e, value) => {
+    const newVolume = parseFloat(value) / 100;
+
+    setVideoState({
+      ...videoState,
+      volume: newVolume,
+      muted: Number(newVolume) === 0 ? true : false, // volume === 0 then muted
+    });
+  };
+
+  const volumeSeekUpHandler = (e, value) => {
+    const newVolume = parseFloat(value) / 100;
+
+    setVideoState({
+      ...videoState,
+      volume: newVolume,
+      muted: newVolume === 0 ? true : false,
+    });
+  };
+
+  const muteHandler = () => {
+    //Mutes the video player
+    setVideoState({ ...videoState, muted: !videoState.muted });
+  };
+
+  const onSeekMouseDownHandler = (e) => {
+    setVideoState({ ...videoState, seeking: true });
+  };
+
+  const mouseMoveHandler = () => {
+    controlRef.current.style.visibility = "visible";
+    count = 0;
+  };
+
+  const bufferStartHandler = () => {
+    console.log("Bufering.......");
+    setVideoState({ ...videoState, buffer: true });
+  };
+
+  const bufferEndHandler = () => {
+    console.log("buffering stoped ,,,,,,play");
+    setVideoState({ ...videoState, buffer: false });
+  };
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="video_container">
+      <div>
+        <h2>Droite</h2>
+        {/* 2024.02.03 */}
+      </div>
+      <Container maxWidth="md" justify="center">
+        <div className="player__wrapper" onMouseMove={mouseMoveHandler}>
+          <ReactPlayer
+            ref={videoPlayerRef}
+            className="player"
+            url="https://vjs.zencdn.net/v/oceans.mp4"
+            width="100%"
+            height="100%"
+            playing={playing}
+            volume={volume}
+            muted={muted}
+            onProgress={progressHandler}
+            onBuffer={bufferStartHandler}
+            onBufferEnd={bufferEndHandler}
+          />
+
+          {/* {buffer && <p>Loading</p>} */}
+
+          <Control
+            controlRef={controlRef}
+            onPlayPause={playPauseHandler}
+            playing={playing}
+            onRewind={rewindHandler}
+            onForward={handleFastFoward}
+            played={played}
+            onSeek={seekHandler}
+            onSeekMouseUp={seekMouseUpHandler}
+            volume={volume}
+            onVolumeChangeHandler={volumeChangeHandler}
+            onVolumeSeekUp={volumeSeekUpHandler}
+            mute={muted}
+            onMute={muteHandler}
+            playRate={playbackRate}
+            duration={formatDuration}
+            currentTime={formatCurrentTime}
+            onMouseSeekDown={onSeekMouseDownHandler}
+          />
+        </div>
+      </Container>
     </div>
   );
 }
